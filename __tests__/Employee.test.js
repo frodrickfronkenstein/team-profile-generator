f4bf09dc4b4ccb4a4fc9f845d40bf05ea4d397fc
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-07-20 07:10:49 -0400

    employee id and email test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,9 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object',() => {
-    const employee = new Employee('Joseph');
+    const employee = new Employee('Joseph', '1', 'Joseph@gmail.com');
 
     expect(employee.name).toBe('Joseph');
+    expect(employee.id).toEqual('1');
+    expect(employee.email).toBe('Joseph@gmail.com');
 });
