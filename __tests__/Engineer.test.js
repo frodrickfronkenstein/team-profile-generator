0132a284cbba30dc2496e0bbcef592d97a99ff88
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-07-20 07:58:20 -0400

    engineer github username test written and passed

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,12 @@
+const Engineer = require('../lib/Engineer');
+
+//github username
+test('get engineer gitHub username', () => {
+    const engineer = new Engineer('Bill', '2', 'bill@gmail.com', 'billsgithubusername');
+
+    expect(engineer.github).toBe('billsgithubusername');
+});
+
+//getGithub()
+
+//getRole()// Overridden to return'Engineer'
