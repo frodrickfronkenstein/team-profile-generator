977cad6410e17fe1d76e186008cf247c8c3ff676
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-07-20 09:04:16 -0400

    out of time

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 const inquirer = require('inquirer');
 
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+let staff = [];
+
+function addStaff(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'what is the name of the manager?',
+        },
+        {
+            type: 'input',
+            name: 'managerID',
+            message: 'what is the employee id of the manager?',
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'what is the email of the manager?'
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the office number?',
+        },
+    ])
+    .then(({ managerName, managerID, managerEmail, officeNumber }) => {
+        let manager = new Manager(managerName, managerID, managerEmail, officeNumber);
+        
+        team.push(manager);
+
+        addEngineer();
+    });
+}
+
+function 
