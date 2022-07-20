666bc0328d507bd031cfa787fd505f3c0566d4c5
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-07-20 07:21:51 -0400

    tests written for character functions

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,27 @@ test('creates an employee object',() => {
     expect(employee.id).toEqual('1');
     expect(employee.email).toBe('Joseph@gmail.com');
 });
+
+test('getName', () => {
+    const employee = new Employee('Joseph', 'Joseph@gmail.com,', '1');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+});
+
+test('getId', () => {
+    const employee = new Employee('Joseph', 'Joseph@gmail.com,', '1');
+
+    expect(employee.getId()).toBe(employee.id);
+});
+
+test('getEmail', () => {
+    const employee = new Employee('Joseph', 'Joseph@gmail.com,', '1');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+})
+
+test('getRole', () => {
+    const employee = new Employee('Joseph', 'Joseph@gmail.com,', '1');
+
+    expect(employee.getRole()).toBe('Employee');
+})//Returns 'Employee'
